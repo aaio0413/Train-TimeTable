e1075915dc04eb6e28db082c1bccf6534a335410
AAIO1992 <takahiro04kawaguchi13@gmail.com>
2018-07-04 15:46:44 -0700

    Made design better. it just needs to have javascript now.

--- assets/javascript/dial.js
+++ assets/javascript/dial.js
@@ -3,3 +3,33 @@ datetimeLocal.on('focus', function (event) {
     this.type = 'datetime-local';
     this.focus();
 });
+
+
+
+var config = {
+apiKey: "AIzaSyCGIhsz7MZpUMNSfl1x0vbixXOfd5AJzHk",
+authDomain: "train-dial.firebaseapp.com",
+databaseURL: "https://train-dial.firebaseio.com",
+projectId: "train-dial",
+storageBucket: "",
+messagingSenderId: "1008326484759"
+};
+firebase.initializeApp(config);
+
+var dataRef = firebase.database();
+
+var trainName = "";
+var destination = "";
+var firstTrain = 0;
+var frequency = 0;
+var nextArrival = 0;
+var minAway = 0;
+
+
+$('#add-train-btn').on('click', function(event) {
+    trainName = $("#train-name").val().trim();
+    destination = $("#destination").val().trim();
+    firstTrain = $("#first-train").val().trim();
+    frequency = $('#frequency').val().trim();
+    comment = $("#comment-input").val().trim();
+})
