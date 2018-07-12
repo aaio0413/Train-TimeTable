d2174740a568e86efb860014dfd3912257946875
AAIO1992 <takahiro04kawaguchi13@gmail.com>
2018-07-12 04:07:43 -0700

    Fixed delete buttons and javascript that updates html change associated with firebase updates. It's finally done!

--- assets/javascript/dial.js
+++ assets/javascript/dial.js
@@ -4,7 +4,6 @@ datetimeLocal.on('focus', function (event) {
     this.focus();
 });
 
-
 var config = {
     apiKey: "AIzaSyCGIhsz7MZpUMNSfl1x0vbixXOfd5AJzHk",
     authDomain: "train-dial.firebaseapp.com",
@@ -15,16 +14,16 @@ var config = {
 };
 
 firebase.initializeApp(config);
-
 var dataRef = firebase.database();
 
 var trainName = "";
 var destination = "";
 var firstTrain = 0;
 var frequency = 0;
-var nextArrival = 0;
 var minAway = 0;
-
+var count = 0;
+var key;
+var idCount=0;
 
 $('#add-train-btn').on('click', function(event) {
     event.preventDefault();
@@ -33,19 +32,30 @@ $('#add-train-btn').on('click', function(event) {
     destination = $("#destination").val().trim();
     firstTrain = $("#first-train").val().trim();
     frequency = $('#frequency').val().trim();
-    // comment = $("#comment-input").val().trim();
 
+    if (!('idCount' in localStorage)) {
+        console.log('idCount does not exist in local storage');
+        localStorage.setItem("idCount", count);
+    } else {
+        count = parseInt(localStorage.getItem('idCount')) + 1;
+        // localStorage.setItem('idCount', idCount++);
+        localStorage.setItem("idCount", count);
+        
+        console.log(localStorage.getItem('idCount'));
+    }
 
+    idCount = parseInt(localStorage.getItem('idCount'));
 
     // First Time (pushed back 1 year to make sure it comes before current time)
     var firstTimeConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
     var tRemainder = diffTime % frequency;
     var tMinutesTillTrain = frequency - tRemainder;
-
     var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+
     console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
     nextArrival = moment(nextTrain).format('hh:mm');
+
 
     dataRef.ref().push({
         trainName: trainName,
@@ -53,21 +63,55 @@ $('#add-train-btn').on('click', function(event) {
         firstTrain: firstTrain,
         frequency: frequency,
         nextArrival: nextArrival,
-        minAway: tMinutesTillTrain
-    });
+        minAway: tMinutesTillTrain,
+        count: idCount
+    })
+
 });
 
-
-  // Firebase watcher + initial loader HINT: .on("value")
 dataRef.ref().on("child_added", function(childSnapshot) {
+    itemCount = childSnapshot.val().count;
 
-    $("#table-body").append("<tr class='table-content'><td class='train-name'> " + childSnapshot.val().trainName +
+    $("#table-body").append("<tr class='table-content' id='" + itemCount + "'><td class='train-name'> " + childSnapshot.val().trainName +
       " </td><td class='destination'> " + childSnapshot.val().destination +
         " </td><td class='frequency'> " + childSnapshot.val().frequency + 
           " </td><td class='next-arrival'> " + childSnapshot.val().nextArrival + 
-          " </td><td class='min-away'> " + childSnapshot.val().minAway + "</td></tr>");
+          " </td><td class='min-away'> " + childSnapshot.val().minAway + 
+          " </td><td><button type='submit' class='delete-item-btn' id='" + itemCount + "'>Delete</button></td></tr>");
 
   // Handle the errors
   }, function(errorObject) {
     console.log("Errors handled: " + errorObject.code);
+});
+
+
+$('#delete-btn').on('click', function() {
+    dataRef.ref().remove();
+    console.log('success?');
+    $('#table-body').empty();
+
+    localStorage.clear();
+    count = 0;
+});
+
+
+$(document).on('click', '.delete-item-btn', function() {
+    var item = parseInt($(this).attr('id'), 10);
+    var buttonNum = (item +0);
+
+    console.log('button is clicked, the itemCount for this item is: ' + buttonNum);
+    alert(buttonNum);
+
+    dataRef.ref().orderByChild('count').equalTo(buttonNum).on("child_added", function(snapshot) {
+        console.log(snapshot.key);
+        key = snapshot.key;
+    });
+
+    dataRef.ref().child(key).remove();
+    console.log('success?');
+
+    var removeHtml = $('.table-content#' + buttonNum);
+    removeHtml.remove();
+
+
 });
